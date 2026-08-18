029ac1ff8acb09de9110104a8eef7f83f0d63edc
izaias98 <izaiascarvalho056@gmail.com>
2026-08-18 20:10:53 -0300

    Adiciona .gitignore

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = \
-    "postgresql://postgres:SUA_SENHA@localhost/arandu"
-
-db = SQLAlchemy(app)
-
 @app.route("/")
 def home():
-    return "Conectado ao PostgreSQL!"
+    return "AranDu Online 🚀"
 
 if __name__ == "__main__":
     app.run(debug=True)
