ce90ad14c99c51dfeae33fa5d42de41e836321b1
izaias98 <izaiascarvalho056@gmail.com>
2026-08-18 21:19:12 -0300

    Backend Flask conectado ao PostgreSQL

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from database import db
+from models import Usuario
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "postgresql://postgres:250a@127.0.0.1:5432/Arandu"
+)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
 @app.route("/")
 def home():
-    return "AranDu Online 🚀"
+    return "AranDu conectado ao PostgreSQL!"
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ backend/models.py
@@ -0,0 +1,8 @@
+from database import db
+
+class Usuario(db.Model):
+    __tablename__ = "usuarios"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
