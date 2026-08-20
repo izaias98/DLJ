dfe6ea82ed81037d9c01bc1f3bc20a15434cb3ec
izaias98 <izaiascarvalho056@gmail.com>
2026-08-20 07:36:42 -0300

    Primeiro cadastro funcionando no PostgreSQL

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from database import db
 from models import Usuario
 
@@ -15,9 +15,29 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+
 @app.route("/")
 def home():
     return "AranDu conectado ao PostgreSQL!"
+
+
+@app.route("/usuarios", methods=["POST"])
+def criar_usuario():
+
+    dados = request.get_json()
+
+    novo_usuario = Usuario(
+        nome=dados["nome"],
+        email=dados["email"]
+    )
+
+    db.session.add(novo_usuario)
+    db.session.commit()
+
+    return jsonify({
+        "mensagem": "Usuário cadastrado com sucesso!"
+    })
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/models.py
+++ backend/models.py
@@ -6,3 +6,10 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "nome": self.nome,
+            "email": self.email
+        }
